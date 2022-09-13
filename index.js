8676f8093220040edcb1ac8d25b7d36ccb0ae6b9
Brooke Ross <brooke.ross2800@gmail.com>
2022-09-12 19:14:26 -0600

    added more CSS and got the fetch request to log an array of cats. Also added to HTML formatting

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-fetch('https://api.thecatapi.com/v1/images/search?api_key=live_cG4QRIvo3Ig0eDPabHxusnqYCqwkypPg4JxYoLAnJ6dxDGSdjSYjGMulIeUSttx8')
+fetch('https://api.thecatapi.com/v1/images/search?api_key=live_cG4QRIvo3Ig0eDPabHxusnqYCqwkypPg4JxYoLAnJ6dxDGSdjSYjGMulIeUSttx8&limit=9')
 .then(res => res.json())
-.then(data=>{console.log(data)})
+.then(data=>{
+    console.log(data)
+})
